cf38a8efbb3a71343c2a11b49f1b67778b480bee
Mark Cotter <markcotter2001@gmail.com>
2022-11-25 17:32:08 +0000

    Created test method for getter for red value in ColourTest.java. Then generated getRed method to be tested in Colour.java

--- src/test/java/com/example/demo/Colour.java
+++ src/test/java/com/example/demo/Colour.java
@@ -1,6 +1,18 @@
 package com.example.demo;
 
+
+
+
 public class Colour {
-    public Colour(double red, double green, double blue) {
+    float red;
+    float green;
+    float blue;
+    public Colour(double red, double green, double blue){
+
+
+    }
+
+    public float getRed() {
+        return 0;
     }
 }
--- src/test/java/com/example/demo/ColourTest.java
+++ src/test/java/com/example/demo/ColourTest.java
@@ -1,12 +1,17 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ColourTest {
+    float redValue;
     @Test
     void testColour() {
        Colour colourTester =new Colour(.4,.3,.7);
-    }
+       redValue=colourTester.getRed();
 
     }
+
+
+}
 
