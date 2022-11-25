b15fff0a233e0411e5ae031beabe872b9732a2fd
Mark Cotter <markcotter2001@gmail.com>
2022-11-25 20:17:47 +0000

    Created assert to test if value of the last 8 digits of the binary sequence which represented the blue value was equal to the value the getter returned for colourTester2.The assert failed

--- src/test/java/com/example/demo/ColourTest.java
+++ src/test/java/com/example/demo/ColourTest.java
@@ -24,8 +24,10 @@ public class ColourTest {
         Colour colourTester2= new Colour("001100110101010101010101");
         redValue=colourTester2.getRed();
         greenValue=colourTester2.getGreen();
+        blueValue=colourTester2.getBlue();
         Assertions.assertEquals((float)(parseInt("00110011",2))/255,redValue);
         Assertions.assertEquals((float)(parseInt("01010101",2))/255,greenValue);
+        Assertions.assertEquals((float)(parseInt("01010101",2))/255,blueValue);
 
 
 
