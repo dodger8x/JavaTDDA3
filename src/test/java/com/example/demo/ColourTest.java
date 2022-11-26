d011407fb2d602c0ab92cfb570fd4fd0b1778602
Mark Cotter <markcotter2001@gmail.com>
2022-11-26 11:56:14 +0000

    Created new test method testColourEqualsMethod. Created assert to test that 2 colours with the same values were equal. The assert failed

--- src/test/java/com/example/demo/ColourTest.java
+++ src/test/java/com/example/demo/ColourTest.java
@@ -17,6 +17,7 @@ public class ColourTest {
        Assertions.assertEquals((float).3,greenValue);
        blueValue=colourTester.getBlue();
        Assertions.assertEquals((float).7,blueValue);
+
     }
 
     @Test
@@ -28,9 +29,13 @@ public class ColourTest {
         Assertions.assertEquals((float)(parseInt("00110011",2))/255,redValue);
         Assertions.assertEquals((float)(parseInt("01010101",2))/255,greenValue);
         Assertions.assertEquals((float)(parseInt("01010101",2))/255,blueValue);
+    }
 
-
-
+    @Test
+    void testColourEqualsMethod() {
+        Colour equalsTester1= new Colour(.4,.3,.7);
+        Colour equalsTester2= new Colour(.4,.3,.7);
+        Assertions.assertTrue(equalsTester1==equalsTester2);
     }
 }
 
